76391a065cdb9d08151fbf46b79aabbfa3ebc184
Carlos <chdezmar@gmail.com>
2016-06-07 12:01:44 +0100

    Html + atributes

--- app.rb
+++ app.rb
@@ -1,5 +1,19 @@
 require "sinatra"
 
 get '/' do
-  'Hello world! Carlos'
+  'Hello!'
+end
+
+get '/secret' do
+  'Secret'
+end
+
+get '/shotgun' do
+  'Testing shotgun'
+end
+
+get "/cat" do
+  "<div style='border: 3px dashed red'>
+    <img src='http://bit.ly/1eze8aE'/>
+    </div>"
 end
