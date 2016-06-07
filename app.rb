e54f78ca464af31459622be242963b3f7dc88f24
Alex-Swann <alex_swann@hotmail.co.uk>
2016-06-07 15:12:16 +0100

    chapter 12

--- app.rb
+++ app.rb
@@ -4,24 +4,10 @@ get '/' do
   "hello!"
 end
 
-get '/random-cat' do
-  @name = ["Amigo", "Oscar", "Viking"].sample
-  erb :index
-end
-
-get '/named-cat' do
-  p params[:name]
-  @name = params[:name]
-  erb :index
-end
-
 get '/secret' do
 
 end
 
-get '/name' do
-  'name'
-end
 
 get "/random-cat" do
   @name = ["Amigo", "Oscar", "Viking"].sample
@@ -32,3 +18,4 @@ end
 get "/named-cat" do
   @name = params[:name]
   erb :index
+end
